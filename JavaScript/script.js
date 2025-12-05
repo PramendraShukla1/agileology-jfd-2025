40b48dc47997800c3345a8fa724c8caa4319f157
Pramendra Shukla <pramendrashukla@Pramendras-MacBook-Air.local>
2025-12-05 09:05:47 +0530

    Day 7 Javascript

---
+++ JavaScript/script.js
@@ -0,0 +1,47 @@
+// var a = 10
+// console.log("old value "+a)
+
+// var a = 20
+// console.log("new value "+ a)
+
+// Global Scope Variable
+
+
+
+// Block Scope Variable
+// function cube(){
+//     var innerFuncVar = 20;
+// }
+// innerFuncVar= 30;
+// console.log(innerFuncVar)
+
+// let b = 20;
+
+// console.log(b)
+
+
+// const c = 20;
+// console.log("old c value "+c)
+// c = 30
+// console.log("new c value "+c)
+
+
+//Numeric Data Types
+// Number
+var a = 10
+console.log(typeof(a))
+
+var name = "Pramendra"
+console.log(typeof(name))
+
+var Condit = true
+console.log(typeof(Condit))
+
+let notAssign;
+console.log(typeof(notAssign))
+
+let noValue = null
+console.log(typeof(noValue))
+
+let bigint = 123456789902837465n
+console.log(typeof(bigint))
